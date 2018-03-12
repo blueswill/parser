8782295dfeada1717a7e3784fd17a666c21701f0
swhc <yy_swhc@outlook.com>
2018-03-12 23:12:03 +0800

    finish dfa

--- src/finite_automata.cc
+++ src/finite_automata.cc
@@ -1,6 +1,7 @@
 #include"finite_automata.hpp"
 
 #include<stack>
+#include<unordered_map>
 
 namespace parser {
     template<typename T>
@@ -10,13 +11,13 @@ namespace parser {
 
     template<typename T>
     std::vector<typename nfa<T>::NodeIndex>
-    nfa<T>::node::transform(const T &cond) {
+    nfa<T>::node::transform(const T &cond) const {
         auto res = trans.equal_range(cond);
         std::vector<NodeIndex> v;
         for (auto iter = res.first; iter != res.second; ++iter) {
             v.push_back(iter->second);
         }
-        return std::move(v);
+        return v;
     }
 
     template<typename T>
@@ -59,6 +60,57 @@ namespace parser {
     }
 
     template<typename T>
+    template<typename Iter>
+    std::vector<typename nfa<T>::NodeIndex> nfa<T>::get_closure(Iter start, Iter end) const {
+        std::stack<NodeIndex> st;
+        for (auto &i = start; i != end; ++i) {
+            st.push(*i);
+        }
+        std::set<NodeIndex> res;
+        while (st.size()) {
+            auto node = st.top();
+            st.pop();
+            if (res.find(node) != res.end()) {
+                continue;
+            }
+            res.insert(node);
+            for (auto &item : nodes[node]->nils) {
+                st.push(item);
+            }
+        }
+        return std::vector<NodeIndex>(res.begin(), res.end());
+    }
+
+    template<typename T>
+    typename nfa<T>::iterator nfa<T>::get_iterator() const {
+        if (starts.empty()) {
+            throw std::runtime_error("no start point");
+        }
+        auto &&res = get_closure(starts.begin(), starts.end());
+        return std::move(iterator(*this, res.begin(), res.end()));
+    }
+
+    template<typename T>
+    bool nfa<T>::contain_transform(const Node &st, const T &cond, const Node &ed) const {
+        auto &found = st->trans.equal_range(cond);
+        for (auto &iter = found.first; iter != found.second; ++iter) {
+            if (iter->second == ed) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    template<typename T>
+    typename nfa<T>::iterator &nfa<T>::iterator::operator=(iterator iter) {
+        if (&this->nfa_ref != &iter.nfa_ref) {
+            throw std::runtime_error("operator= on non-compatible objects");
+        }
+        this->current = std::move(iter.current);
+        return *this;
+    }
+
+    template<typename T>
     bool nfa<T>::iterator::in_end() const {
         for (auto &item : current) {
             if (nfa_ref.ends.find(item) == nfa_ref.ends.end()) {
@@ -70,31 +122,37 @@ namespace parser {
 
     template<typename T>
     bool nfa<T>::iterator::transform(const T &cond) {
-        std::set<NodeIndex> nodes;
-        std::stack<NodeIndex> st;
+        auto &&iter = get_transform(cond);
+        *this = iter;
+        return current.size();
+    }
+
+    template<typename T>
+    typename nfa<T>::iterator nfa<T>::iterator::get_transform(const T &cond) const {
+        std::vector<NodeIndex> res;
+        for (auto &item : current) {
+            std::vector<NodeIndex> its(std::move(nfa_ref.nodes[item]->transform(cond)));
+            res.insert(res.end(), its.begin(), its.end());
+        }
+        auto &&cur = nfa_ref.get_closure(res.begin(), res.end());
+        return iterator(nfa_ref, cur.begin(), cur.end());
+    }
+
+    template<typename T>
+    typename std::set<T> nfa<T>::iterator::all_transforms() const {
+        std::set<T> result;
         auto &ref = nfa_ref.nodes;
         for (auto &item : current) {
-            std::vector<NodeIndex> its(std::move(ref[item]->transform(cond)));
-            for (auto &i : its) {
-                st.push(i);
+            for (auto &con : ref[item]->trans) {
+                result.insert(con.first);
             }
         }
+        return std::move(result);
+    }
 
-        //closure dfs
-        while (st.size()) {
-            NodeIndex cur = st.top();
-            st.pop();
-            if (nodes.find(cur) != nodes.end()) {
-                continue;
-            }
-            nodes.insert(cur);
-            for (auto &item : ref[cur]->nils) {
-                st.push(item);
-            }
-        }
-
-        current = std::move(nodes);
-        return current.size();
+    template<typename T>
+    inline bool operator==(const typename nfa<T>::iterator &iter1, const typename nfa<T>::iterator &iter2) {
+        return iter1.nfa_ref == iter2.nfa_ref && iter1.current == iter2.current;
     }
 
     template<typename T>
@@ -105,4 +163,76 @@ namespace parser {
         }
         return seed;
     }
+
+    template<typename T>
+    nfa<T> dfa<T>::transform_to_dfa(const nfa<T> &nfa_ref) {
+        std::unordered_map<iterator, std::pair<typename nfa<T>::Node, bool>, typename nfa<T>::iterator::hasher> record;
+        nfa<T> result;
+
+        //dfs nfa->dfa
+        std::stack<iterator> st;
+        auto &&iter = nfa_ref.get_iterator();
+        record.insert({iter, {result.get_new_node(), false}});
+        st.push(iter);
+        while (st.size()) {
+            iterator cur = st.top();
+            st.pop();
+            auto &p = record[cur];
+            if (p.second == true) {
+                continue;
+            }
+            for (auto &con : cur.all_transforms()) {
+                iterator &&tmp = cur.get_transform(con);
+                auto found = record.find(tmp);
+                typename nfa<T>::Node node;
+                if (found == record.end()) {
+                    node = result.get_new_node();
+                    record.insert({tmp, {node, false}});
+                }
+                else {
+                    node = found->second.first;
+                }
+                if (result.contain_transform(cur, con, node)) {
+                    continue;
+                }
+                result.add_transform(cur, con, node);
+            }
+            record[cur] = std::make_pair(p.first, true);
+        }
+        return result;
+    }
 }
+
+#ifdef DEBUG
+
+#include<vector>
+#include<iostream>
+parser::nfa<int> graph;
+std::vector<parser::nfa<int>::Node> nodes(4);
+
+#define ADDNIL(x,y) (graph.add_transform(nodes[x], nodes[y]))
+#define ADD(x,y,c) (graph.add_transform(nodes[x],c,nodes[y]))
+
+int main(void) {
+    for (auto &i : nodes) {
+        i = graph.get_new_node();
+    }
+    ADD(0,1,1);
+    ADD(0,1,3);
+    ADDNIL(0,1);
+    ADD(0,2,2);
+    ADD(0,3,4);
+    ADD(0,3,1);
+    ADD(1,3,6);
+    ADD(3,1,5);
+    ADDNIL(1,2);
+    graph.set_start(nodes[0]);
+    parser::nfa<int>::iterator iter = graph.get_iterator();
+    iter.transform(1);
+    for (auto i : iter.all_transforms()) {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
+
+#endif
--- src/include/finite_automata.hpp
+++ src/include/finite_automata.hpp
@@ -10,9 +10,10 @@ namespace parser {
 
     template<typename T>
     class nfa {
+    public:
+        class iterator;
     private:
         typedef int NodeIndex;
-        class iterator;
         friend class iterator;
         /*
           null connection
@@ -29,22 +30,33 @@ namespace parser {
             void add(const T &cond, NodeIndex ind);
             void add(NodeIndex ind);
 
-            node() = default;
-        public:
-            std::vector<NodeIndex> transform(const T &cond);
+            std::vector<NodeIndex> transform(const T &cond) const;
         };
         std::set<NodeIndex> starts;
         std::set<NodeIndex> ends;
         std::vector<std::shared_ptr<node>> nodes;
+
+        template<typename Iter>
+        std::vector<NodeIndex> get_closure(Iter start, Iter end) const;
     public:
         typedef std::shared_ptr<node> Node;
         inline void add_transform(const Node &st, const T &cond, const Node &ed);
         inline void add_transform(const Node &st, const Node &ed); //unconditional
-        void set_start(const Node &st);
-        void set_end(const Node &ed);
+        inline void set_start(const Node &st);
+        inline void set_end(const Node &ed);
 
-        Node get_new_node();
+        inline Node get_new_node();
+
+        inline iterator get_iterator() const;
+    protected:
+        inline bool contain_transform(const Node &st, const T &cond, const Node &ed) const;
     };
+
+    template<typename T>
+    bool operator==(const typename nfa<T>::iterator &iter1, const typename nfa<T>::iterator &iter2);
+
+    template<typename T>
+    class dfa;
 
     template<typename T>
     class nfa<T>::iterator {
@@ -53,24 +65,43 @@ namespace parser {
             std::size_t operator()(const iterator &iter);
         };
 
-        std::set<NodeIndex> current;
-        nfa<T> &nfa_ref;
         friend struct hasher;
-    public:
-        iterator(nfa<T> &nf, const std::set<NodeIndex> &s) :
-            nfa_ref(nf), current(s) {}
+        friend class nfa<T>;
+        friend class dfa<T>;
 
-        iterator(nfa<T> &nf, std::set<NodeIndex> &&s) :
-            nfa_ref(nf), current(s) {}
+        const nfa<T> &nfa_ref;
+        std::vector<NodeIndex> current;
+
+        template<typename Iter>
+        iterator(const nfa<T> &nf, Iter start, Iter end) :
+            nfa_ref(nf), current(start, end) {}
+    public:
+        iterator(iterator &&iter) :
+            nfa_ref(iter.nfa_ref), current(std::move(iter.current)) {}
+        iterator(const iterator &iter) :
+            nfa_ref(iter.nfa_ref), current(iter.current) {}
+        inline iterator &operator=(iterator right);
+        friend bool operator==<T>(const iterator &iter1, const iterator &iter2);
         inline bool in_end() const;
         /*
           true: there is connection available
           false: otherwise
         */
         bool transform(const T &cond);
+        iterator get_transform(const T &cond) const;
+        inline std::set<T> all_transforms() const;
     };
 
-
+    template<typename T>
+    class dfa : public nfa<T> {
+    public:
+        class iterator;
+    private:
+        static nfa<T> transform_to_dfa(const nfa<T> &nfa_ref);
+    public:
+        dfa(const nfa<T> &nfa_ref) :
+            nfa<T>(transform_to_dfa(nfa_ref)) {}
+    };
 }
 
 #endif
